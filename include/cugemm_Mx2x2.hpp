ac08dea4288a885a0dd27f8768e74455f5359924
mutsuki <mutsuki@momo86.net>
2022-10-14 11:30:15 +0900

    Add gemm_strided_batch_Mx2x2

--- include/cugemm_Mx2x2.hpp
+++ include/cugemm_Mx2x2.hpp
@@ -15,6 +15,18 @@ void gemm_Mx2x2(
 		const T beta,
 		T* const c_ptr, const std::size_t ldc
 		);
+template <class T>
+void gemm_strided_batch_Mx2x2(
+		const cublasOperation_t op_a,
+		const cublasOperation_t op_b,
+		const unsigned M,
+		const T alpha,
+		const T* const a_ptr, const std::size_t lda, const std::size_t stridea,
+		const T* const b_ptr, const std::size_t ldb, const std::size_t strideb,
+		const T beta,
+		T* const c_ptr, const std::size_t ldc, const std::size_t stridec,
+		const unsigned batch_count
+		);
 } // namespace cugemm
 } // namespace mtk
 
--- src/main.cu
+++ src/main.cu
@@ -193,6 +193,47 @@ void gemm_internal(
 		gemm_kernel<T, LAYOUT_A, LAYOUT_B, BLOCK_SIZE, M_PER_THREAD, true , N, K><<<grid_size, BLOCK_SIZE>>>(M, alpha, a_ptr, lda, b_ptr, ldb, beta, c_ptr, ldc);
 	}
 }
+
+template <class T, class LAYOUT_A, class LAYOUT_B, unsigned BLOCK_SIZE, unsigned M_PER_THREAD, bool BETA, unsigned N = 2, unsigned K = 2>
+__global__ void gemm_strided_batch_kernel(
+		const unsigned M,
+		const T alpha,
+		const T* const a_ptr, const std::size_t lda, const std::size_t stridea,
+		const T* const b_ptr, const std::size_t ldb, const std::size_t strideb,
+		const T beta,
+		T* const c_ptr, const std::size_t ldc, const std::size_t stridec
+		) {
+	gemm_core<T, LAYOUT_A, LAYOUT_B, BLOCK_SIZE, M_PER_THREAD, BETA, N, K>(
+			M,
+			alpha,
+			a_ptr + stridea * blockDim.y, lda,
+			b_ptr + strideb * blockDim.y, ldb,
+			beta,
+			c_ptr + stridec * blockDim.y, ldc
+			);
+}
+
+template <class T, class LAYOUT_A, class LAYOUT_B, unsigned BLOCK_SIZE, unsigned M_PER_THREAD, unsigned N = 2, unsigned K = 2>
+void gemm_strided_batch_internal(
+		const unsigned M,
+		const T alpha,
+		const T* const a_ptr, const std::size_t lda, const std::size_t stridea,
+		const T* const b_ptr, const std::size_t ldb, const std::size_t strideb,
+		const T beta,
+		T* const c_ptr, const std::size_t ldc, const std::size_t stridec,
+		const std::size_t batch_count
+		) {
+	assert(M >= M_PER_THREAD);
+	assert((M & (M - 1)) == 0);
+	const auto min_grid_size = std::max<unsigned>(M / (M_PER_THREAD * BLOCK_SIZE), 1u);
+	const auto grid_size = dim3(min_grid_size, batch_count);
+
+	if (detail::is_zero(beta)) {
+		gemm_strided_batch_kernel<T, LAYOUT_A, LAYOUT_B, BLOCK_SIZE, M_PER_THREAD, false, N, K><<<grid_size, BLOCK_SIZE>>>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec);
+	} else {
+		gemm_strided_batch_kernel<T, LAYOUT_A, LAYOUT_B, BLOCK_SIZE, M_PER_THREAD, true , N, K><<<grid_size, BLOCK_SIZE>>>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec);
+	}
+}
 } // unnamed namespace
 
 template <>
@@ -240,5 +281,55 @@ void mtk::cugemm::gemm_Mx2x2<cuComplex>(
 		gemm_internal<cuComplex, col_major, row_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, b_ptr, ldb, beta, c_ptr, ldc);
 	} else if (op_a == CUBLAS_OP_T && op_b == CUBLAS_OP_N) {
 		gemm_internal<cuComplex, row_major, row_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, b_ptr, ldb, beta, c_ptr, ldc);
+	}
+}
+
+template <>
+void mtk::cugemm::gemm_strided_batch_Mx2x2<float>(
+		const cublasOperation_t op_a,
+		const cublasOperation_t op_b,
+		const unsigned M,
+		const float alpha,
+		const float* const a_ptr, const std::size_t lda, const std::size_t stridea,
+		const float* const b_ptr, const std::size_t ldb, const std::size_t strideb,
+		const float beta,
+		float* const c_ptr, const std::size_t ldc, const std::size_t stridec,
+		const unsigned batch_count
+		) {
+	constexpr unsigned M_PER_THREAD = 4;
+	constexpr unsigned BLOCK_SIZE = 256;
+	if (op_a == CUBLAS_OP_N && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<float, col_major, col_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	} else if (op_a == CUBLAS_OP_T && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<float, row_major, col_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	} else if (op_a == CUBLAS_OP_N && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<float, col_major, row_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	} else if (op_a == CUBLAS_OP_T && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<float, row_major, row_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	}
+}
+
+template <>
+void mtk::cugemm::gemm_strided_batch_Mx2x2<cuComplex>(
+		const cublasOperation_t op_a,
+		const cublasOperation_t op_b,
+		const unsigned M,
+		const cuComplex alpha,
+		const cuComplex* const a_ptr, const std::size_t lda, const std::size_t stridea,
+		const cuComplex* const b_ptr, const std::size_t ldb, const std::size_t strideb,
+		const cuComplex beta,
+		cuComplex* const c_ptr, const std::size_t ldc, const std::size_t stridec,
+		const unsigned batch_count
+		) {
+	constexpr unsigned M_PER_THREAD = 4;
+	constexpr unsigned BLOCK_SIZE = 256;
+	if (op_a == CUBLAS_OP_N && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<cuComplex, col_major, col_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	} else if (op_a == CUBLAS_OP_T && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<cuComplex, row_major, col_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	} else if (op_a == CUBLAS_OP_N && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<cuComplex, col_major, row_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
+	} else if (op_a == CUBLAS_OP_T && op_b == CUBLAS_OP_N) {
+		gemm_strided_batch_internal<cuComplex, row_major, row_major, BLOCK_SIZE, M_PER_THREAD>(M, alpha, a_ptr, lda, stridea, b_ptr, ldb, strideb, beta, c_ptr, ldc, stridec, batch_count);
 	}
 }
